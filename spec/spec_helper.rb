ab263e22bd06549554eed80254a2f58d4dd6fabb
Alexander Lazarov <alexander.lazaroff@gmail.com>
2015-06-22 14:13:18 +0300

    add spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'rspec'
+require 'active_model'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'country_flags'
