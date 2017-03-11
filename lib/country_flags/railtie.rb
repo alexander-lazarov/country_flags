08a0c7e5635acb3cc9a50a84e61f2f2d5a045ea1
Alexander Lazarov <alexander.lazaroff@gmail.com>
2017-03-11 17:26:04 +0200

    Add images to assets precompile

--- lib/country_flags/railtie.rb
+++ lib/country_flags/railtie.rb
@@ -4,6 +4,7 @@ module CountryFlags
   class Railtie < ::Rails::Railtie
     initializer 'country_flags.view_helpers' do
       ActionView::Base.send :include, Helper
+      Rails.application.config.assets.precompile += %w(country_flags/png/* country_flags/gif/*)
     end
   end
 end
